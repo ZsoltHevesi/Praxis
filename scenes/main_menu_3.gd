7e3ec55e9bf898662ccd8312594d399a26fd22f8
teniolajoda100 <124061707+teniolajoda100@users.noreply.github.com>
2025-03-16 18:42:43 +0200

    Back button in scenes

--- scenes/main_menu_3.gd
+++ scenes/main_menu_3.gd
@@ -17,3 +17,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu2.tscn")
--- scripts/main_menu_2.gd
+++ scripts/main_menu_2.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_motorskills_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu3.tscn")
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
