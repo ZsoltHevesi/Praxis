8653a2db249ec4ef506bc3916c868f90a2ce2973
teniolajoda100 <124061707+teniolajoda100@users.noreply.github.com>
2025-03-30 16:56:42 +0300

    functionality

--- scripts/main_menu_2.gd
+++ scripts/main_menu_2.gd
@@ -8,5 +8,9 @@ func _on_button_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
-func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Info2.tscn")
+#func _on_button_2_pressed() -> void:
+	# get_tree().change_scene_to_file("res://scenes/Info2.tscn") 
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Info2.tscn") 
