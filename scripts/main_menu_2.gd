4bcff0df1244837ea88499558b3e471b391c7b82
teniolajoda100 <124061707+teniolajoda100@users.noreply.github.com>
2025-03-30 16:14:36 +0300

    second info page

--- scripts/main_menu_2.gd
+++ scripts/main_menu_2.gd
@@ -6,3 +6,7 @@ func _on_motorskills_pressed() -> void:
 
 func _on_button_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Info2.tscn")
